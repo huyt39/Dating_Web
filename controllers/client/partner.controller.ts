2c239f393c69b1708445b2bd718c91f47e30e12e
Huy Tuan <hotuanhuy1203@gmail.com>
2024-06-04 09:38:59 +0700

    updating

--- controllers/client/partner.controller.ts
+++ controllers/client/partner.controller.ts
@@ -1,17 +1,13 @@
-
-
 import { Request, Response } from "express";
 import Partner from "../../models/partner.model";
 import sequelize from "../../config/database";
 import { QueryTypes } from "sequelize";
 import { parse } from "path";
-
-//[GET] /tours/slugCategory
+//[GET] /partners/slugCategory
 export const index = async (req: Request, res: Response)=>{
     const slugCategory=req.params.slugCategory;
     
-
-    const partners = await sequelize.query(`
+    const partners= await sequelize.query(`
     SELECT partners.*
     FROM partners
     JOIN partners_categories ON partners.id=partners_categories.partner_id
@@ -25,26 +21,23 @@ export const index = async (req: Request, res: Response)=>{
     `,{
         type: QueryTypes.SELECT //dinh nghia kieu truy van
     }); //truy van tu db
-
     partners.forEach(item => {
         if(item["images"]){
             const images=JSON.parse(item["images"]); //chuyen anh tu json sang array
             item["image"] = images[0];
         }
-
         
     })
-
     console.log(partners);
-
     res.render("client/pages/partners/index.pug", {
         pageTitle: "Danh sách partners",
         partners: partners
     });
 }
-//[GET] /partners/detail/:slugTour
+//[GET] /partners/detail/:slugPartner
 export const detail = async (req: Request, res: Response)=>{
     const slugPartner=req.params.slugPartner;
+   
     const partnerDetail=await Partner.findOne({
         where:{
             slug: slugPartner,
@@ -57,11 +50,10 @@ export const detail = async (req: Request, res: Response)=>{
     if(partnerDetail["images"]){
         partnerDetail["images"]=JSON.parse(partnerDetail["images"]); //chuyen anh tu json ve array
     }
-    
-    console.log(partnerDetail);
+
     res.render("client/pages/partners/detail", {
         pageTitle: "Chi tiết partner",
         partnerDetail: partnerDetail
-        
+
     });
 }
