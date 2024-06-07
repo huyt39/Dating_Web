ff2639ecc6fe026c3388db2cbd2d4cd18593b8b4
Huy Tuan <hotuanhuy1203@gmail.com>
2024-06-07 15:34:03 +0700

    cart layout

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,9 +2,11 @@
 import { Express } from "express";
 import { partnerRoutes } from "./partner.route";
 import { categoryRoutes } from "./category.route";
+import { cartRoutes } from "./cart.route";
 
 const clientRoutes=(app: Express): void => {
     app.use(`/partners`, partnerRoutes);
     app.use(`/categories`, categoryRoutes);
+    app.use(`/cart`, cartRoutes );
 };
 export default clientRoutes;
